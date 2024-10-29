4bfbdeec388ff4cb8b538da942f6a001039ec612
Christian Heel <66922325+heelc29@users.noreply.github.com>
2024-10-29 20:12:05 +0100

    [5.3] add router service provider / inject dispatcher in mvc factory and database driver

--- installation/includes/app.php
+++ installation/includes/app.php
@@ -40,11 +40,13 @@ if (is_writable(JPATH_ADMINISTRATOR . '/logs')) {
 // Register the Installation application
 JLoader::registerNamespace('Joomla\\CMS\\Installation', JPATH_INSTALLATION . '/src', false, false);
 
+// @todo: remove?
 JLoader::registerAlias('JRouterInstallation', \Joomla\CMS\Installation\Router\InstallationRouter::class);
 
 // Get the dependency injection container
 $container = \Joomla\CMS\Factory::getContainer();
-$container->registerServiceProvider(new \Joomla\CMS\Installation\Service\Provider\Application());
+$container->registerServiceProvider(new \Joomla\CMS\Installation\Service\Provider\Application())
+    ->registerServiceProvider(new \Joomla\CMS\Installation\Service\Provider\Router());
 
 /*
  * Alias the session service keys to the web session service as that is the primary session backend for this application
