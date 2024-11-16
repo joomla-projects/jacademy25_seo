5ff9d13450f467d6b16262074d00194fd70def2c
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2024-11-16 10:08:56 +0100

    Add extension class to joomlaupdater

---
+++ administrator/components/com_joomlaupdate/src/Extension/JoomlaupdateComponent.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_joomlaupdate
+ *
+ * @copyright   (C) 2024 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Component\Joomlaupdate\Administrator\Extension;
+
+use Joomla\CMS\Extension\MVCComponent;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+/**
+ * Component class for com_config
+ *
+ * @since  __DEPLOY_VERSION__
+ */
+class JoomlaupdateComponent extends MVCComponent
+{
+}
