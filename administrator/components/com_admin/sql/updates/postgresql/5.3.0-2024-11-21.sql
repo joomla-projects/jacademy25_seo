31c8bfc86695452734bed913cb9a94f02d9d695e
BrianTeeman <brian@teeman.net>
2024-11-21 21:25:54 +0000

    post
    Signed-off-by: BrianTeeman <brian@teeman.net>

--- administrator/components/com_admin/sql/updates/postgresql/5.3.0-2024-11-21.sql
+++ administrator/components/com_admin/sql/updates/postgresql/5.3.0-2024-11-21.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "#__contact_details" ADD COLUMN "image_alt" varchar(255) AFTER "image" /** CAN FAIL **/;
-ALTER TABLE "#__contact_details" ADD COLUMN "image_alt_empty" smallint NOT NULL DEFAULT 0 AFTER "image_alt" /** CAN FAIL **/;
+ALTER TABLE "#__contact_details" ADD COLUMN "image_alt" varchar(255) /** CAN FAIL **/;
+ALTER TABLE "#__contact_details" ADD COLUMN "image_alt_empty" smallint NOT NULL DEFAULT 0 /** CAN FAIL **/;
