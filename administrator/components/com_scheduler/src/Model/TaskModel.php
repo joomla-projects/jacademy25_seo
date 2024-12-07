e154d76acfc270ed4170f4ade00c63c853d6bfe8
Allon Moritz <allon.moritz@digital-peak.com>
2024-12-07 17:14:04 +0100

    [6.0] The admin model does return a stdClass instead of CMSObject (#42961)
    Thanks @laoneo

--- administrator/components/com_scheduler/src/Model/TaskModel.php
+++ administrator/components/com_scheduler/src/Model/TaskModel.php
@@ -322,14 +322,14 @@ class TaskModel extends AdminModel
         }
 
         // Parent call leaves `execution_rules` and `cron_rules` JSON encoded
-        $item->set('execution_rules', json_decode($item->get('execution_rules', '')));
-        $item->set('cron_rules', json_decode($item->get('cron_rules', '')));
+        $item->execution_rules = json_decode($item->execution_rules ?? '');
+        $item->cron_rules      = json_decode($item->cron_rules ?? '');
 
         $taskOption = SchedulerHelper::getTaskOptions()->findOption(
             ($item->id ?? 0) ? ($item->type ?? 0) : $this->getState('task.type')
         );
 
-        $item->set('taskOption', $taskOption);
+        $item->taskOption = $taskOption;
 
         return $item;
     }
