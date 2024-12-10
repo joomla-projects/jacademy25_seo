495c8e140c36ee0c4d068b7efea328412e02d5fd
BrianTeeman <brian@teeman.net>
2024-12-10 21:12:07 +0000

    use
    Signed-off-by: BrianTeeman <brian@teeman.net>

--- components/com_content/tmpl/archive/default_items.php
+++ components/com_content/tmpl/archive/default_items.php
@@ -12,7 +12,6 @@ defined('_JEXEC') or die;
 
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Layout\LayoutHelper;
-use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
 use Joomla\Component\Content\Site\Helper\RouteHelper;
 
