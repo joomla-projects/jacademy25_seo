d97a148be17ebb024ec445d99303aa2042d6a037
Allon Moritz <allon.moritz@digital-peak.com>
2024-12-13 10:04:14 +0100

    Add rector

---
+++ build/rector.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * @package   DPDocker
+ * @copyright Copyright (C) 2024 Digital Peak GmbH. <https://www.digital-peak.com>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
+ */
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\LogicalAnd\LogicalToBooleanRector;
+use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
+use Rector\CodingStyle\Rector\Use_\SeparateMultiUseImportsRector;
+use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
+use Rector\DeadCode\Rector\If_\UnwrapFutureCompatibleIfPhpVersionRector;
+use Rector\DeadCode\Rector\StaticCall\RemoveParentCallWithoutParentRector;
+use Rector\EarlyReturn\Rector\If_\ChangeOrIfContinueToMultiContinueRector;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+use Rector\ValueObject\PhpVersion;
+
+return static function (RectorConfig $rectorConfig): void {
+    // Min version
+    $rectorConfig->phpVersion(PhpVersion::PHP_81);
+
+    // No short class import
+    $rectorConfig->importShortClasses(false);
+
+    // Tabs for indentation
+    $rectorConfig->indent(' ', 4);
+
+    // Complete sets with rules
+    $rectorConfig->sets([
+        //LevelSetList::UP_TO_PHP_81,
+        //SetList::CODE_QUALITY,
+        //SetList::CODING_STYLE,
+        //SetList::DEAD_CODE,
+        //SetList::EARLY_RETURN,
+        //SetList::INSTANCEOF,
+        //SetList::NAMING,
+        //SetList::PRIVATIZATION,
+        //SetList::STRICT_BOOLEANS,
+        //SetList::TYPE_DECLARATION,
+    ]);
+
+    // Skip some rules and folders/files
+    $rectorConfig->skip([
+        // Keep Joomla version compare
+        UnwrapFutureCompatibleIfPhpVersionRector::class,
+        // Keep docs
+        RemoveUselessParamTagRector::class,
+        RemoveUselessReturnTagRector::class,
+        // Keep <?php } ? on same line
+        NewlineAfterStatementRector::class,
+        // Do not remove methods and properties
+        RemoveUnusedPrivateMethodRector::class,
+        // Stay safe and do not remove code
+        RemoveParentCallWithoutParentRector::class,
+        // Keep the or in JEXEC
+        LogicalToBooleanRector::class,
+        // No splitting if with ||
+        ChangeOrIfContinueToMultiContinueRector::class,
+        // Multiuse should be allowed in component classes
+        SeparateMultiUseImportsRector::class => ['*Component.php'],
+
+        // Ignore vendor
+        '*/vendor/*',
+    ]);
+
+    // The bootstrap file, which finds the core classes and loads the extension namespace
+    $rectorConfig->bootstrapFiles([__DIR__ . '/phpstan/joomla-bootstrap.php']);
+};
