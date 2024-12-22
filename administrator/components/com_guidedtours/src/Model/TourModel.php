bfe8dcc654f02feefc007ff9b1ba404134d78fd1
Hannes Papenberg <info@joomlager.de>
2024-12-22 09:36:07 +0100

    Fixing comments

--- administrator/components/com_guidedtours/src/Model/TourModel.php
+++ administrator/components/com_guidedtours/src/Model/TourModel.php
@@ -220,7 +220,7 @@ class TourModel extends AdminModel
             return false;
         }
 
-        // Convert to the CMSObject before adding other data.
+        // Convert to an object before adding other data.
         $properties = $table->getProperties(1);
         $item       = ArrayHelper::toObject($properties);
 
