b9dc60d2f433bd81fe37a3d02d0ae0f46e8f063b
alikon <optimus4joomla@gmail.com>
2025-01-07 12:50:21 +0100

    cs

--- tests/System/integration/cli/User.cy.js
+++ tests/System/integration/cli/User.cy.js
@@ -1,20 +1,19 @@
 describe('Test that console command user', () => {
-    it('can list users', () => {
-      cy.exec(`php ${Cypress.env('cmsPath')}/cli/joomla.php user:list`)
-        .its('stdout')
-        .should('contain', 'ci-admin');
-    });
-    it('can add a user', () => {
-      const para = '--username=test --name=test --password=123456789012 --email=test@530.test --usergroup=manager -n';
-      cy.exec(`php ${Cypress.env('cmsPath')}/cli/joomla.php user:add ${para}`)
-        .its('stdout')
-        .should('contain', 'User created!');
-    });
-    it('can delete a user', () => {
-      const para = '--username=test -n';
-      cy.exec(`php ${Cypress.env('cmsPath')}/cli/joomla.php user:delete ${para}`)
-        .its('stdout')
-        .should('contain', 'User test deleted!');
-    });
-  });
-  
+	it('can list users', () => {
+		cy.exec(`php ${Cypress.env('cmsPath')}/cli/joomla.php user:list`)
+			.its('stdout')
+			.should('contain', 'ci-admin');
+	});
+	it('can add a user', () => {
+		const para = '--username=test --name=test --password=123456789012 --email=test@530.test --usergroup=manager -n';
+		cy.exec(`php ${Cypress.env('cmsPath')}/cli/joomla.php user:add ${para}`)
+			.its('stdout')
+			.should('contain', 'User created!');
+	});
+	it('can delete a user', () => {
+		const para = '--username=test -n';
+		cy.exec(`php ${Cypress.env('cmsPath')}/cli/joomla.php user:delete ${para}`)
+			.its('stdout')
+			.should('contain', 'User test deleted!');
+	});
+});
