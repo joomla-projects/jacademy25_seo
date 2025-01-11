13dce26fed749f053939e52d5192c8a93f62a3a7
Nicola Galgano <optimus4joomla@gmail.com>
2025-01-11 12:43:57 +0100

    cs

--- administrator/components/com_users/src/Helper/DebugHelper.php
+++ administrator/components/com_users/src/Helper/DebugHelper.php
@@ -85,7 +85,6 @@ class DebugHelper
             $component_actions = Access::getActionsFromFile(JPATH_ADMINISTRATOR . '/components/' . $component . '/access.xml');
 
             if (!empty($component_actions)) {
-
                 // Load language
                 $lang      = Factory::getApplication()->getLanguage();
                 $lang->load($component, JPATH_ADMINISTRATOR, null, false, false);
