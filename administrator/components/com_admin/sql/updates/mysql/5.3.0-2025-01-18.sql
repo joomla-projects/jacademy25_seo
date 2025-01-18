bb3be61156d3813ea0f2b73453ae9f939dae8509
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2025-01-18 16:03:09 +0100

    Implement quickicon

---
+++ administrator/components/com_admin/sql/updates/mysql/5.3.0-2025-01-18.sql
@@ -0,0 +1,3 @@
+INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `custom_data`, `ordering`, `state`)
+SELECT 0, 'plg_quickicon_autoupdate', 'plugin', 'autoupdate', 'quickicon', 0, 1, 1, 0, 1, '', '', '', -1, 0
+WHERE NOT EXISTS (SELECT * FROM `#__extensions` e WHERE e.`type` = 'plugin' AND e.`element` = 'autoupdate' AND e.`folder` = 'quickicon' AND e.`client_id` = 0);
