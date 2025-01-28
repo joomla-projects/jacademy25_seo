577b942aa17cc1dde3ae531ed9ba65485ef4685b
Christiane Maier-Stadtherr <dev@chmst.de>
2025-01-28 10:07:30 +0100

    Update administrator/components/com_content/src/Controller/DisplayController.php
    Co-authored-by: Quy Ton <quy@nomonkeybiz.com>

--- administrator/components/com_content/src/Controller/DisplayController.php
+++ administrator/components/com_content/src/Controller/DisplayController.php
@@ -62,7 +62,7 @@ class DisplayController extends BaseController
             return false;
         }
 
-        if ($view == 'featured') {
+        if ($view === 'featured') {
             $this->setRedirect(Route::_('index.php?option=com_content&view=articles&featured=1', false));
             return false;
         }
