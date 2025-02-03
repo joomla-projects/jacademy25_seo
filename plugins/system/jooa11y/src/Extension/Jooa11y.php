26641ccdfef1cea871c22346ff9903f07eb064d0
Adam Chaboryk <adam.chaboryk@gmail.com>
2025-02-03 10:20:37 -0500

    Update Jooa11y to Sa11y 4.0.3
    - Updated Sa11y from 3.1.3 to 4.0.3
    - Removed the following setting fields: Contrast, Form Labels, Links (Advanced), Colour Filter, Web Components to Check, Additional Checks Always On
    - Unused language strings moved to end of file (to be removed with 6.0)
    - Introduced new "Turn Off Checks" field, where admins can turn off specific checks within Sa11y by key name.

--- plugins/system/jooa11y/src/Extension/Jooa11y.php
+++ plugins/system/jooa11y/src/Extension/Jooa11y.php
@@ -27,200 +27,202 @@ use Joomla\Event\SubscriberInterface;
  */
 final class Jooa11y extends CMSPlugin implements SubscriberInterface
 {
-    /**
-     * Subscribe to certain events
-     *
-     * @return string[]  An array of event mappings
-     *
-     * @since 4.1.0
-     *
-     * @throws \Exception
-     */
-    public static function getSubscribedEvents(): array
-    {
-        if (Factory::getApplication()->isClient('site')) {
-            return ['onAfterRoute' => ['initJooa11y', Priority::HIGH]];
-        }
-
-        return [];
+  /**
+   * Subscribe to certain events
+   *
+   * @return string[]  An array of event mappings
+   *
+   * @since 4.1.0
+   *
+   * @throws \Exception
+   */
+  public static function getSubscribedEvents(): array
+  {
+    if (Factory::getApplication()->isClient('site')) {
+      return ['onAfterRoute' => ['initJooa11y', Priority::HIGH]];
     }
 
-    /**
-     * Method to check if the current user is allowed to see the debug information or not.
-     *
-     * @return  boolean  True if access is allowed.
-     *
-     * @since   4.1.0
-     */
-    private function isAuthorisedDisplayChecker(): bool
-    {
-        static $result;
+    return [];
+  }
 
-        if ($result !== null) {
-            return $result;
-        }
+  /**
+   * Method to check if the current user is allowed to see the debug information or not.
+   *
+   * @return  boolean  True if access is allowed.
+   *
+   * @since   4.1.0
+   */
+  private function isAuthorisedDisplayChecker(): bool
+  {
+    static $result;
 
-        $result = true;
-
-        // If the user is not allowed to view the output then end here.
-        $filterGroups = (array) $this->params->get('filter_groups', []);
-
-        if (!empty($filterGroups)) {
-            $userGroups = $this->getApplication()
-                ->getIdentity()
-                ->get('groups');
-
-            if (!array_intersect($filterGroups, $userGroups)) {
-                $result = false;
-            }
-        }
-
-        return $result;
+    if ($result !== null) {
+      return $result;
     }
 
-    /**
-     * Init the checker.
-     *
-     * @return  void
-     *
-     * @since   4.1.0
-     */
-    public function initJooa11y()
-    {
-        // Check if we are in a preview modal or the plugin has enforced loading
-        $showJooa11y = $this->getApplication()
-            ->getInput()
-            ->get('jooa11y', $this->params->get('showAlways', 0));
+    $result = true;
 
-        // Load the checker if authorised
-        if (!$showJooa11y || !$this->isAuthorisedDisplayChecker()) {
-            return;
-        }
+    // If the user is not allowed to view the output then end here.
+    $filterGroups = (array) $this->params->get('filter_groups', []);
 
-        // Disable page cache
-        $this->getDispatcher()->addListener(
-            'onPageCacheSetCaching',
-            static function (SetCachingEvent $event) {
-                $event->addResult(false);
-            }
-        );
+    if (!empty($filterGroups)) {
+      $userGroups = $this->getApplication()
+        ->getIdentity()
+        ->get('groups');
 
-        // Register own event to add the checker later, once a document is created
-        $this->getDispatcher()->addListener('onBeforeCompileHead', [$this, 'addJooa11y']);
+      if (!array_intersect($filterGroups, $userGroups)) {
+        $result = false;
+      }
     }
 
-    /**
-     * Add the checker.
-     *
-     * @return  void
-     *
-     * @since   __DEPLOY_VERSION__
-     */
-    public function addJooa11y()
-    {
-        // Load translations
-        $this->loadLanguage();
+    return $result;
+  }
 
-        // Detect the current active language
-        $getLang = $this->getApplication()
-            ->getLanguage()
-            ->getTag();
+  /**
+   * Init the checker.
+   *
+   * @return  void
+   *
+   * @since   4.1.0
+   */
+  public function initJooa11y()
+  {
+    // Check if we are in a preview modal or the plugin has enforced loading
+    $showJooa11y = $this->getApplication()
+      ->getInput()
+      ->get('jooa11y', $this->params->get('showAlways', 0));
 
-        // Get the right locale
-        $splitLang = explode('-', $getLang);
-        $lang      = $splitLang[0];
-        $country   = $splitLang[1] ?? '';
-
-        // Sa11y is available in the following languages
-        $supportedLang = [
-            'bg',
-            'cs',
-            'da',
-            'de',
-            'el',
-            'en',
-            'es',
-            'et',
-            'fi',
-            'fr',
-            'hu',
-            'id',
-            'it',
-            'ja',
-            'ko',
-            'lt',
-            'lv',
-            'nb',
-            'nl',
-            'pl',
-            'pt',
-            'ro',
-            'sl',
-            'sk',
-            'sv',
-            'tr',
-            'uk',
-            'ua',
-            'zh',
-        ];
-
-        // Check if Sa11y supports language
-        if (!\in_array($lang, $supportedLang)) {
-            $lang = 'en';
-        } elseif ($lang === 'pt') {
-            $lang = $country === 'BR' ? 'ptBR' : 'ptPT';
-        } elseif ($lang === 'uk') {
-            $lang = 'ua';
-        } elseif ($lang === 'en') {
-            $lang = $country === 'US' ? 'enUS' : 'en';
-        }
-
-        // Get the document object
-        /** @var \Joomla\CMS\Document\HtmlDocument $document */
-        $document = $this->getApplication()->getDocument();
-
-        // Get plugin options from xml
-        $getOptions = [
-            'checkRoot'           => $this->params->get('checkRoot', 'main'),
-            'readabilityRoot'     => $this->params->get('readabilityRoot', 'main'),
-            'containerIgnore'     => $this->params->get('containerIgnore'),
-            'contrastPlugin'      => $this->params->get('contrastPlugin', 1),
-            'formLabelsPlugin'    => $this->params->get('formLabelsPlugin', 1),
-            'linksAdvancedPlugin' => $this->params->get('linksAdvancedPlugin', 1),
-            'colourFilterPlugin'  => $this->params->get('colourFilterPlugin', 1),
-            'checkAllHideToggles' => $this->params->get('additionalChecks', 0),
-            'shadowComponents'    => $this->params->get('shadowComponents'),
-        ];
-        $getExtraProps = $this->params->get('extraProps', []);
-
-
-        // Process extra props
-        $extraProps = [];
-        foreach ($getExtraProps as $prop) {
-            $decodedValue = json_decode($prop->value);
-            if (is_numeric($decodedValue) || \is_bool($decodedValue)) {
-                $extraProps[$prop->key] = $decodedValue;
-            } else {
-                $extraProps[$prop->key] = "{$prop->value}";
-            }
-        }
-
-        // Merge all options together and add to page
-        $allOptions = array_merge($getOptions, $extraProps);
-        $document->addScriptOptions('jooa11yOptions', $allOptions);
-
-        /** @var \Joomla\CMS\WebAsset\WebAssetManager $wa*/
-        $wa = $document->getWebAssetManager();
-        $wa->getRegistry()->addExtensionRegistryFile('plg_system_jooa11y');
-
-        // Load scripts and instantiate
-        $wa->useStyle('sa11y')
-            ->useScript('sa11y')
-            ->registerAndUseScript(
-                'sa11y-lang',
-                'vendor/sa11y/' . $lang . '.js',
-                ['importmap' => true]
-            )
-            ->useScript('plg_system_jooa11y.jooa11y');
+    // Load the checker if authorised
+    if (!$showJooa11y || !$this->isAuthorisedDisplayChecker()) {
+      return;
     }
+
+    // Disable page cache
+    $this->getDispatcher()->addListener(
+      'onPageCacheSetCaching',
+      static function (SetCachingEvent $event) {
+        $event->addResult(false);
+      }
+    );
+
+    // Register own event to add the checker later, once a document is created
+    $this->getDispatcher()->addListener('onBeforeCompileHead', [$this, 'addJooa11y']);
+  }
+
+  /**
+   * Add the checker.
+   *
+   * @return  void
+   *
+   * @since   __DEPLOY_VERSION__
+   */
+  public function addJooa11y()
+  {
+    // Load translations
+    $this->loadLanguage();
+
+    // Detect the current active language
+    $getLang = $this->getApplication()
+      ->getLanguage()
+      ->getTag();
+
+    // Get the right locale
+    $splitLang = explode('-', $getLang);
+    $lang      = $splitLang[0];
+    $country   = $splitLang[1] ?? '';
+
+    // Sa11y is available in the following languages
+    $supportedLang = [
+      'bg',
+      'cs',
+      'da',
+      'de',
+      'el',
+      'en',
+      'es',
+      'et',
+      'fi',
+      'fr',
+      'hu',
+      'id',
+      'it',
+      'ja',
+      'ko',
+      'lt',
+      'lv',
+      'nb',
+      'nl',
+      'pl',
+      'pt',
+      'ro',
+      'sl',
+      'sk',
+      'sv',
+      'tr',
+      'uk',
+      'ua',
+      'zh',
+    ];
+
+    // Check if Sa11y supports language
+    if (!\in_array($lang, $supportedLang)) {
+      $lang = 'en';
+    } elseif ($lang === 'pt') {
+      $lang = $country === 'BR' ? 'ptBR' : 'ptPT';
+    } elseif ($lang === 'uk') {
+      $lang = 'ua';
+    } elseif ($lang === 'en') {
+      $lang = $country === 'US' ? 'enUS' : 'en';
+    }
+
+    // Get the document object
+    /** @var \Joomla\CMS\Document\HtmlDocument $document */
+    $document = $this->getApplication()->getDocument();
+
+    // Get plugin options from xml
+    $getOptions = [
+      'checkRoot'           => $this->params->get('checkRoot', 'main'),
+      'readabilityRoot'     => $this->params->get('readabilityRoot', 'main'),
+      'containerIgnore'     => $this->params->get('containerIgnore'),
+    ];
+    $getExtraProps = $this->params->get('extraProps', []);
+    $getChecks = $this->params->get('checks', []);
+
+    // Process Sa11y's props
+    function processProps($props)
+    {
+      $result = [];
+      foreach ($props as $prop) {
+        $decodedValue = json_decode($prop->value);
+        if (is_numeric($decodedValue) || \is_bool($decodedValue)) {
+          $result[$prop->key] = $decodedValue;
+        } else {
+          $result[$prop->key] = "{$prop->value}";
+        }
+      }
+      return $result;
+    }
+    $extraProps = processProps($getExtraProps);
+    $checks = processProps($getChecks);
+    $allChecks = ['checks' => $checks];
+
+
+    // Merge all options together and add to page
+    $allOptions = array_merge($getOptions, $extraProps, $allChecks);
+    $document->addScriptOptions('jooa11yOptions', $allOptions);
+
+    /** @var \Joomla\CMS\WebAsset\WebAssetManager $wa*/
+    $wa = $document->getWebAssetManager();
+    $wa->getRegistry()->addExtensionRegistryFile('plg_system_jooa11y');
+
+    // Load scripts and instantiate
+    $wa->useStyle('sa11y')
+      ->useScript('sa11y')
+      ->registerAndUseScript(
+        'sa11y-lang',
+        'vendor/sa11y/' . $lang . '.js',
+        ['importmap' => true]
+      )
+      ->useScript('plg_system_jooa11y.jooa11y');
+  }
 }
