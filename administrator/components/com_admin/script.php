56b72248a133daecac1d4c7afc1bd45832cf4c80
Richard Fath <richard.fath@t-online.de>
2025-02-07 15:42:14 +0100

    Add deleted files from PR #44812

--- administrator/components/com_admin/script.php
+++ administrator/components/com_admin/script.php
@@ -2369,6 +2369,8 @@ class JoomlaInstallerScript
             '/media/system/css/system-site-offline_rtl.min.css.gz',
             '/media/system/scss/system-site-offline_rtl.scss',
             // From 5.3.0-alpha3 to 5.3.0-beta1
+            '/libraries/vendor/guzzlehttp/promises/src/functions.php',
+            '/libraries/vendor/guzzlehttp/promises/src/functions_include.php',
             '/libraries/vendor/maximebf/debugbar/LICENSE',
             '/libraries/vendor/maximebf/debugbar/src/DebugBar/Bridge/CacheCacheCollector.php',
             '/libraries/vendor/maximebf/debugbar/src/DebugBar/Bridge/DoctrineCollector.php',
