a462eee839993c5c0f37b5dd6d6fdbd9be08cf31
Benjamin Trenkle <benjamin.trenkle@wicked-software.de>
2025-02-22 11:49:59 +0100

    Fix SQL files

--- administrator/components/com_admin/sql/updates/postgresql/6.0.0-2024-08-11.sql
+++ administrator/components/com_admin/sql/updates/postgresql/6.0.0-2024-08-11.sql
@@ -1,6 +1,6 @@
 -- Update link to featured
 
 UPDATE "#__menu"
-SET "link" = 'index.php?option=com_content&view=articles&featured=1'
+SET "link" = 'index.php?option=com_content&view=articles&filter[featured]=1'
 WHERE "link" = 'index.php?option=com_content&view=featured'
 AND "client_id" = 1;
