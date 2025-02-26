ffc0112f7577606332c517938b4156829d2f3bed
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2025-02-26 10:23:14 +0200

    cleanup

--- build/build-modules-js/init/patches.mjs
+++ build/build-modules-js/init/patches.mjs
@@ -26,17 +26,6 @@ export const patchPackages = async (options) => {
   );
   await writeFile(chosenPath, ChosenJs, { encoding: 'utf8', mode: 0o644 });
 
-  // Append initialising code to the end of the Short-and-Sweet javascript
-  dest = join(mediaVendorPath, 'short-and-sweet');
-  const shortandsweetPath = `${dest}/${options.settings.vendors['short-and-sweet'].js['dist/short-and-sweet.min.js']}`;
-  let ShortandsweetJs = await readFile(shortandsweetPath, { encoding: 'utf8' });
-  ShortandsweetJs = ShortandsweetJs.concat(`
-shortAndSweet('textarea.charcount,input.charcount', {counterClassName: 'small text-muted'});
-/** Repeatable */
-document.addEventListener("joomla:updated", (event) => [].slice.call(event.target.querySelectorAll('textarea.charcount,input.charcount')).map((el) => shortAndSweet(el, {counterClassName: 'small text-muted'})));
-`);
-  await writeFile(shortandsweetPath, ShortandsweetJs, { encoding: 'utf8', mode: 0o644 });
-
   // Include the v5 shim for Font Awesome
   const faPath = join(mediaVendorPath, 'fontawesome-free/scss/fontawesome.scss');
   const newScss = (await readFile(faPath, { encoding: 'utf8' })).concat(`
