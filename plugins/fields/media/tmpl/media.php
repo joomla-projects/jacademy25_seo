2f0fc135102ddde16a202aad64ebc10a07d408be
Sergey Tolkachyov <musstudent@mail.ru>
2025-02-27 18:14:11 +0400

    Alpha order namespace import

--- plugins/fields/media/tmpl/media.php
+++ plugins/fields/media/tmpl/media.php
@@ -10,10 +10,10 @@
 
 defined('_JEXEC') or die;
 
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Layout\LayoutHelper;
 use Joomla\CMS\Helper\MediaHelper;
+use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Layout\LayoutHelper;
 
 if (empty($field->value) || empty($field->value['imagefile'])) {
     return;
