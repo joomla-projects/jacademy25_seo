2407a796d9745c3c30f2e05c448392bca296cca8
Sergey Tolkachyov <musstudent@mail.ru>
2025-02-27 13:12:54 +0400

    fix code style for continuous-integration/drone/pr - global_namespace_import

--- libraries/src/Form/Field/AccessiblemediaField.php
+++ libraries/src/Form/Field/AccessiblemediaField.php
@@ -134,7 +134,7 @@ class AccessiblemediaField extends SubformField
     /**
      * Method to attach a Form object to the field.
      *
-     * @param   SimpleXMLElement  $element  The SimpleXMLElement object representing the <field /> tag for the form field object.
+     * @param   \SimpleXMLElement  $element  The SimpleXMLElement object representing the <field /> tag for the form field object.
      * @param   mixed              $value    The form field value to validate.
      * @param   string             $group    The field name group control value.
      *
@@ -142,7 +142,7 @@ class AccessiblemediaField extends SubformField
      *
      * @since   4.0.0
      */
-    public function setup(SimpleXMLElement $element, $value, $group = null)
+    public function setup(\SimpleXMLElement $element, $value, $group = null)
     {
         /**
          * When you have subforms which are not repeatable (i.e. a subform custom field with the
