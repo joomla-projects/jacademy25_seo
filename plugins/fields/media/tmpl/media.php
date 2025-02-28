8e771487b0b38334fb7bba75b7fb390bd93ec038
Sergey Tolkachyov <musstudent@mail.ru>
2025-02-28 10:24:10 +0400

    Add support own layouts for `audios` and `videos` file types.

--- plugins/fields/media/tmpl/media.php
+++ plugins/fields/media/tmpl/media.php
@@ -10,18 +10,54 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Component\ComponentHelper;
 use Joomla\CMS\Helper\MediaHelper;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\LayoutHelper;
+use Joomla\Filesystem\File;
 
 if (empty($field->value) || empty($field->value['imagefile'])) {
     return;
 }
 
 $fileUrl = MediaHelper::getCleanMediaFieldValue($field->value['imagefile']);
-
 $class = $fieldParams->get('image_class');
+$comMediaParams = ComponentHelper::getParams('com_media');
+
+$audiosExt    = array_map(
+    'trim',
+    explode(
+        ',',
+        $comMediaParams->get(
+            'audio_extensions',
+            'mp3,m4a,mp4a,ogg'
+        )
+    )
+);
+$videosExt    = array_map(
+    'trim',
+    explode(
+        ',',
+        $comMediaParams->get(
+            'video_extensions',
+            'mp4,mp4v,mpeg,mov,webm'
+        )
+    )
+);
+$documentsExt = array_map(
+    'trim',
+    explode(
+        ',',
+        $comMediaParams->get(
+            'doc_extensions',
+            'doc,odg,odp,ods,odt,pdf,ppt,txt,xcf,xls,csv'
+        )
+    )
+);
+
+$fileExtension = File::getExt($fileUrl);
+
 $options = [];
 
 if ($class) {
@@ -34,11 +70,17 @@ if (MediaHelper::isImage($fileUrl) || MediaHelper::getMimeType($fileUrl) === 'im
         'alt' => empty($field->value['alt_text']) && empty($field->value['alt_empty']) ? false : $field->value['alt_text'],
     ];
 
-    if ($class) {
-        $options['class'] = $class;
-    }
-
     echo LayoutHelper::render('joomla.html.image', $options);
+} elseif (\in_array($fileExtension, $audiosExt)) {
+    $options['src']      = $fileUrl;
+    $options['controls'] = 'controls';
+
+    echo LayoutHelper::render('joomla.html.audio', $options);
+} elseif (\in_array($fileExtension, $videosExt)) {
+    $options['src']      = $fileUrl;
+    $options['controls'] = 'controls';
+
+    echo LayoutHelper::render('joomla.html.video', $options);
 } else {
     $linkText = $field->value['linktext'] ?? Text::_('JLIB_FORM_FIELD_PARAM_ACCESSIBLEMEDIA_PARAMS_LINKTEXT_DEFAULT_VALUE');
 
