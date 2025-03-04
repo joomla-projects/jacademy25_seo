634646e4a5579fd1ab21a1dcf09a050d4719c9c1
Oleg Voronkovich <oleg-voronkovich@yandex.ru>
2025-03-04 20:16:02 +0300

    Update copyright year to 2025
    Co-authored-by: Brian Teeman <brian@teeman.net>

--- tests/Unit/Libraries/Cms/Service/Provider/ConfigTest.php
+++ tests/Unit/Libraries/Cms/Service/Provider/ConfigTest.php
@@ -4,7 +4,7 @@
  * @package     Joomla.UnitTest
  * @subpackage  Service
  *
- * @copyright   (C) 2013 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2025 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
