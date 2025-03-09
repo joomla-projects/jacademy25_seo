16fc63d0bf1eb144d9ef5c0d8fa83cdf5b8aeeff
Nicola Galgano <optimus4joomla@gmail.com>
2025-03-09 21:23:59 +0100

    Update administrator/components/com_admin/sql/updates/postgresql/5.3.0-2024-12-23.sql
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- administrator/components/com_admin/sql/updates/postgresql/5.3.0-2024-12-23.sql
+++ administrator/components/com_admin/sql/updates/postgresql/5.3.0-2024-12-23.sql
@@ -2,4 +2,4 @@ ALTER TABLE "#__assets" ALTER COLUMN "name" TYPE varchar(255) NOT NULL;
 ALTER TABLE "#__assets" ALTER COLUMN "title" TYPE varchar(255) NOT NULL;
 ALTER TABLE "#__categories" ALTER COLUMN "extension" TYPE varchar(100) DEFAULT '' NOT NULL;
 ALTER TABLE "#__workflows" ALTER COLUMN "extension" TYPE varchar(255) NOT NULL;
-ALTER TABLE "#__workflow_associations" ALTER COLUMN “extension" TYPE varchar(255) NOT NULL;
+ALTER TABLE "#__workflow_associations" ALTER COLUMN "extension" TYPE varchar(255) NOT NULL;
