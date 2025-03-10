e20d16e207748e28d411ca34213d629ac420ed68
krishnaGandhi11 <krishnagandhi0911@gmail.com>
2025-03-11 05:09:59 +0530

    Removed unrequired bootstrap loading

--- templates/cassiopeia/index.php
+++ templates/cassiopeia/index.php
@@ -130,9 +130,6 @@ $wa->getAsset('style', 'fontawesome')->setAttribute('rel', 'lazy-stylesheet');
 <html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 
 <head>
-    <?php
-    HTMLHelper::_('bootstrap.loadCss');
-    ?>
     <jdoc:include type="metas" />
     <jdoc:include type="styles" />
     <jdoc:include type="scripts" />
@@ -258,7 +255,6 @@ $wa->getAsset('style', 'fontawesome')->setAttribute('rel', 'lazy-stylesheet');
     <?php endif; ?>
 
     <jdoc:include type="modules" name="debug" style="none" />
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
