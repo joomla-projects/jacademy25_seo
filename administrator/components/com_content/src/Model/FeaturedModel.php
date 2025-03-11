522827f1afcbb3b1368f0cce9e57de8fccb779f4
Christiane Maier-Stadtherr <dev@chmst.de>
2025-03-11 19:05:08 +0100

    [6.0] Re-add FeaturedModel for correct deprecation (#45015)

--- administrator/components/com_content/src/Model/FeaturedModel.php
+++ administrator/components/com_content/src/Model/FeaturedModel.php
@@ -8,21 +8,21 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-namespace Joomla\Component\Content\Administrator\Controller;
+namespace Joomla\Component\Content\Administrator\Model;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
 // phpcs:enable PSR1.Files.SideEffects
 
 /**
- * Featured content controller class.
+ * Methods supporting a list of featured article records.
  *
  * @since  1.6
  *
  * @deprecated  6.0 will be removed in 8.0
- *              Use \Joomla\Component\Content\Administrator\Controller\ArticlesController instead
- *              For Quickicons use articles.getQuickiconFeatured()
+ *              Use \Joomla\Component\Content\Administrator\Model\ArticlesModel instead
+ *              set the filter.featured state to 1 to get only featured articles
  */
-class FeaturedController extends ArticlesController
+class FeaturedModel extends ArticlesModel
 {
 }
