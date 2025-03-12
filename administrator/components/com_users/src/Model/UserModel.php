5b43327985eed34a004b6404c527c1aa00b670d0
Amitesh007z <145351157+Amitesh007z@users.noreply.github.com>
2025-03-13 01:49:49 +0530

    Update administrator/components/com_users/src/Model/UserModel.php
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>

--- administrator/components/com_users/src/Model/UserModel.php
+++ administrator/components/com_users/src/Model/UserModel.php
@@ -420,7 +420,6 @@ class UserModel extends AdminModel implements UserFactoryAwareInterface
                     // If unblocking, also change password reset count to zero to unblock reset
                     if ($table->block === 0) {
                         $table->resetCount = 0;
-                        //code for sortig manual fix acxtivation issue
                         $table->activation = '';
                     }
 
