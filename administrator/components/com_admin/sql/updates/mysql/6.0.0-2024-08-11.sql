07a6496596628f8e206d5d41483cc30e7382f6d5
TLWebdesign <info@tlwebdesign.nl>
2025-03-24 17:56:25 +0100

    added sql and updated year in language files

--- administrator/components/com_admin/sql/updates/mysql/6.0.0-2024-08-11.sql
+++ administrator/components/com_admin/sql/updates/mysql/6.0.0-2024-08-11.sql
@@ -4,3 +4,6 @@ UPDATE `#__menu`
 SET `link` = 'index.php?option=com_content&view=articles&filter[featured]=1'
 WHERE `link` = 'index.php?option=com_content&view=featured'
 AND `client_id` = 1;
+
+INSERT INTO `#__extensions` (`package_id`, `name`, `type`, `element`, `folder`, `client_id`, `enabled`, `access`, `protected`, `locked`, `manifest_cache`, `params`, `custom_data`, `ordering`, `state`) VALUES
+(0, 'plg_fields_note', 'plugin', 'note', 'fields', 0, 1, 1, 0, 1, '', '{"class":"alert alert-info","heading":"h4"}', '', 18, 0);
