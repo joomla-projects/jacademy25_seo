f515f763a8905f770456458c0200e8bdcf073503
TLWebdesign <info@tlwebdesign.nl>
2025-03-24 17:37:56 +0100

    Note custom field first code

---
+++ plugins/fields/note/tmpl/note.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Fields.Note
+ *
+ * @copyright   (C) 2025 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
