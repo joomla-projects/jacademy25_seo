6bdf7e1c713399a81cdcca1d5682a140edd45e35
TLWebdesign <info@tlwebdesign.nl>
2025-03-24 16:08:05 +0100

    updated sql files to include all the default params settings

--- administrator/components/com_admin/sql/updates/postgresql/6.0.0-2024-08-11.sql
+++ administrator/components/com_admin/sql/updates/postgresql/6.0.0-2024-08-11.sql
@@ -6,4 +6,4 @@ WHERE "link" = 'index.php?option=com_content&view=featured'
 AND "client_id" = 1;
 
 INSERT INTO "#__extensions" ("package_id", "name", "type", "element", "folder", "client_id", "enabled", "access", "protected", "locked", "manifest_cache", "params", "custom_data", "ordering", "state") VALUES
-(0, 'plg_fields_number', 'plugin', 'number', 'fields', 0, 1, 1, 0, 1, '', '{"min":"1.0","max":"100.0","step":"0.1"}', '', 17, 0);
+(0, 'plg_fields_number', 'plugin', 'number', 'fields', 0, 1, 1, 0, 1, '', '{"min":"1.0","max":"100.0","step":"0.1","currency":"0","position":"0","decimals":"2"}', '', 17, 0);
