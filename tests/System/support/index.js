7fbff8c16ce9d49103ce770180bbefb3b00276b1
Allon Moritz <allon.moritz@digital-peak.com>
2025-04-29 10:02:56 +0200

    Does check the server error logs in system tests

--- tests/System/support/index.js
+++ tests/System/support/index.js
@@ -3,9 +3,11 @@ import('joomla-cypress');
 
 before(() => {
   cy.task('startMailServer');
+  cy.task('clearLogs');
 });
 
 afterEach(() => {
   cy.checkForPhpNoticesOrWarnings();
   cy.task('cleanupDB');
+  cy.task('checkForLogs');
 });
