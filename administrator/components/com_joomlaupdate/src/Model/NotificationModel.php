55b346480466ebc93becfa44d5e303f6a609de44
David Jardin <d.jardin@djumla.de>
2025-05-10 17:16:57 +0200

    Apply suggestions from code review
    Co-authored-by: Richard Fath <richard67@users.noreply.github.com>
    Co-authored-by: Brian Teeman <brian@teeman.net>

--- administrator/components/com_joomlaupdate/src/Model/NotificationModel.php
+++ administrator/components/com_joomlaupdate/src/Model/NotificationModel.php
@@ -4,7 +4,7 @@
  * @package     Joomla.Administrator
  * @subpackage  com_joomlaupdate
  *
- * @copyright   (C) 2012 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2025 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
