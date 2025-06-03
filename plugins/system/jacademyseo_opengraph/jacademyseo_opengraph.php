eb2262704983ab91a87edd8b9cf9a57241b49837
shahzan01 <m.shahzan2003@gmail.com>
2025-06-03 20:19:17 +0530

    feat : added manifest and language files for jacademyseo_opengraph plugin

---
+++ plugins/system/jacademyseo_opengraph/jacademyseo_opengraph.php
@@ -0,0 +1,14 @@
+<?php
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Plugin\CMSPlugin;
+
+class PlgContentOgmetadata extends CMSPlugin
+{
+    protected $app;
+
+    public function onContentPrepare($context, &$article, &$params, $limitstart = 0)
+    {
+        // Logic will be added later.
+    }
+}
