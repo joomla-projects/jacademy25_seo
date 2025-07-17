7cfde557e6162549c7851ad8963630ac3113550e
shahzan01 <m.shahzan2003@gmail.com>
2025-07-17 23:14:08 +0530

    feat : og tag injection into html

--- plugins/system/opengraph/src/Extension/opengraph.php
+++ plugins/system/opengraph/src/Extension/opengraph.php
@@ -4,7 +4,7 @@
  * @package     Joomla.Plugin
  * @subpackage  System.Opengraph
  *
- * @copyright   (C) 2024 Open Source Matters, Inc. <https://www.joomla.org>
+ * @copyright   (C) 2025 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -23,9 +23,10 @@ use Joomla\CMS\Document\Document;
 use Joomla\CMS\Opengraph\OpengraphServiceInterface;
 use Joomla\CMS\Uri\Uri;
 use Exception;
-
-
-
+use Joomla\CMS\Table\Content;
+use Joomla\Database\DatabaseInterface;
+use Joomla\CMS\Menu\MenuItem;
+use Joomla\Component\Fields\Administrator\Helper\FieldsHelper;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
@@ -56,6 +57,17 @@ final class Opengraph extends CMSPlugin implements SubscriberInterface
     protected $autoloadLanguage = true;
 
     /**
+     * Database object
+     *
+     * The database is injected by parent constructor
+     *
+     * @var  DatabaseInterface
+     * @since  __DEPLOY_VERSION__
+     */
+    protected $db;
+
+
+    /**
      * Returns an array of events this subscriber will listen to.
      *
      * @return  array
@@ -69,55 +81,10 @@ final class Opengraph extends CMSPlugin implements SubscriberInterface
             'onContentPrepareForm' => 'onContentPrepareForm',
         ];
     }
-    /**
-     * Handle the beforeCompileHead event.
-     *
-     * @param BeforeCompileHeadEvent $event
-     *
-     * @return void
-     */
-    public function onBeforeCompileHead(BeforeCompileHeadEvent $event): void
-    {
-        $app = $this->getApplication();
 
-        if (!$app->isClient('site')) {
-            return;
-        }
-
-        $input = $app->input;
-        $option = $input->get('option', '', 'cmd');
-        $view = $input->get('view', '', 'cmd');
-        $id = $input->getInt('id');
-
-
-        // @todo: will be changed in future to support other components
-
-        if ($option !== 'com_content' || $view !== 'article' || !$id) {
-            return;
-        }
-
-        $table = Table::getInstance('Content');
-        if (!$table->load($id)) {
-            return;
-        }
-
-        $attribs = new Registry($table->attribs);
-
-        if ($attribs->get('og_enabled', '') === '0') {
-            return;
-        }
-
-        $document = $this->app->getDocument();
-
-        if (!$document instanceof HtmlDocument) {
-            return;
-        }
-
-        $this->injectOpenGraphData($document, $attribs);
-    }
 
     /**
-     * Handle the onContentPrepareForm event.
+     * Add fields for the OpenGraph data to the form
      *
      * @param PrepareFormEvent $event
      *
@@ -163,34 +130,330 @@ final class Opengraph extends CMSPlugin implements SubscriberInterface
 
 
     /**
-     * Inject the OpenGraph data into the document.
+     * Handle the beforeCompileHead event.
      *
-     * @param Document $document
-     * @param Registry $params
+     * @param BeforeCompileHeadEvent $event
      *
      * @return void
      */
-    private function injectOpenGraphData(Document $document, Registry $params): void
+    public function onBeforeCompileHead(BeforeCompileHeadEvent $event): void
+    {
+        $app = $this->app;
+
+        if (!$app->isClient('site')) {
+            return;
+        }
+
+        $document = $app->getDocument();
+
+        // Only process HTML documents
+        if (!($document instanceof HtmlDocument)) {
+            return;
+        }
+
+        $input = $app->input;
+        $option = $input->get('option', '', 'cmd');
+        $view = $input->get('view', '', 'cmd');
+        $id = $input->getInt('id');
+
+        // Skip indexer and feed formats
+        if (($option . '.' . $view) === 'com_finder.indexer') {
+            return;
+        }
+
+        if ($input->get('format', '', 'cmd') === 'feed') {
+            return;
+        }
+        // Check if plugin is enabled globally
+        if (!$this->params->get('enable_og_generation', 1)) {
+            return;
+        }
+        $uniqueArticle = true;
+        if (is_array($input->get('id', 0, 'int'))) {
+            $uniqueArticle = false;
+        }
+
+        if ($uniqueArticle) {
+            $article = new Content($this->db);
+            $article->load($id);
+            $category = Table::getInstance('Category');
+            $category->load($article->catid);
+        }
+
+
+
+
+        // Get menu parameters
+        $menuParams = $this->getMenuParams();
+        $articleAttribs = new Registry($article->attribs ?? '{}');
+        $categoryParams = new Registry($category->params ?? '{}');
+        $articleImages = $this->getAllArticleImages(new Registry($article->images ?? '{}'));
+
+
+
+        $ogTags = [
+            'og_title' => '',
+            'og_description' => '',
+            'og_image' => '',
+            'og_image_alt' => '',
+            'og_type' => '',
+            'og_url' => '',
+            'twitter_card' => '',
+            'twitter_title' => '',
+            'twitter_description' => '',
+            'twitter_image' => '',
+            'twitter_image_alt' => '',
+            'fb_app_id' => '',
+            'site_name' => '',
+            'url' => '',
+            'base_url' => ''
+        ];
+
+        // Get Global settings
+
+        $config = $this->app->getConfig();
+
+        $ogTags['fb_app_id'] = $this->params->get('fb_app_id');
+        $ogTags['site_name'] = $config->get('sitename');
+        $ogTags['base_url'] = Uri::base();
+        $ogTags['url'] = Uri::getInstance()->toString();
+
+        //  get OG tags from category mappings
+        $this->getOgTagsFromCategoryMappings($categoryParams, $article, $articleImages, $ogTags);
+
+        //  get OG tags from article form
+        $this->getOgTagsFromParams($articleAttribs, $ogTags);
+
+        //  get OG tags from menu form
+        $this->getOgTagsFromParams($menuParams, $ogTags);
+
+        //  get Twitter tags
+        $this->getTwitterOgTags($ogTags);
+
+        // Inject the OpenGraph data into the document
+        $this->injectOpenGraphData($document, $ogTags);
+    }
+
+
+    /**
+     * Generates OG metadata values based on category field mapping and article data.
+     *
+     * @param Registry $categoryParams The category params containing OG field mappings.
+     * @param object $article The article object.
+     * @param array $articleImages The array of article images.
+     * @param array $ogTags The array of OG tags.
+     */
+    private function getOgTagsFromCategoryMappings(Registry $categoryParams, Content $article, array $articleImages, array &$ogTags): void
+    {
+
+        foreach ($categoryParams as $key => $fieldName) {
+            // Only process keys that start with 'og_'
+            if (strpos($key, 'og_') === 0 && str_ends_with($key, '_field')) {
+                $ogTagName = substr($key, 0, -6); // Remove "_field" from the end
+
+                $ogTags[$ogTagName] = $this->getFieldValue($article, $fieldName, $articleImages);
+            }
+        }
+    }
+
+
+
+    /**
+     * Get value from article field or custom field
+     *
+     * @param Content $article
+     * @param string $fieldName
+     * @param array $articleImages
+     *
+     * @return string
+     */
+    private function getFieldValue(Content $article, string $fieldName, array $articleImages): string
+    {
+        // Check if it's a custom field
+        if (strpos($fieldName, 'field.') === 0) {
+            $customFieldName = substr($fieldName, 6);
+            // Load custom fields for the article
+            $customFields = FieldsHelper::getFields('com_content.article', $article, true);
+
+            foreach ($customFields as $field) {
+
+                if ($field->name == $customFieldName) {
+
+                    return $field->value ?? '';
+                }
+            }
+
+            return '';
+        }
+
+        // Handle standard article fields
+        $value = '';
+
+        switch ($fieldName) {
+            case 'title':
+                $value = $article->title;
+                break;
+
+            case 'alias':
+                $value = $article->alias;
+                break;
+
+            case 'articletext':
+                $value = $article->introtext . $article->fulltext;
+                break;
+
+            case 'metadesc':
+                $value = $article->metadesc;
+                break;
+
+            case 'metakey':
+                $value = $article->metakey;
+                break;
+
+            case 'image_intro':
+                $value = $articleImages['image_intro'];
+                break;
+
+            case 'image_fulltext':
+                $value = $articleImages['image_fulltext'];
+                break;
+
+            case 'image_intro_alt':
+                $value = $articleImages['image_intro_alt'];
+                break;
+
+            case 'image_fulltext_alt':
+                $value = $articleImages['image_fulltext_alt'];
+                break;
+
+            case 'created_by_alias':
+                $value = $article->created_by_alias;
+                break;
+
+            default:
+                if (property_exists($article, $fieldName)) {
+                    $value = $article->$fieldName;
+                }
+        }
+
+        return (string) $value;
+    }
+
+
+
+    /**
+     * @param   Registry  $articleImages
+     *
+     * @return array
+     * @since  __DEPLOY_VERSION__
+     */
+    private function getAllArticleImages(Registry $articleImages): array
+    {
+        $image_intro = $image_intro_alt = '';
+        $image_fulltext = $image_fulltext_alt = '';
+
+        // Handle image_intro
+        if ($articleImages->get('image_intro') > '') {
+            // get part before # in image if # exists
+            $image_intro = strpos($articleImages->get('image_intro'), '#') !== false
+                ? substr($articleImages->get('image_intro'), 0, strpos($articleImages->get('image_intro'), '#'))
+                : $articleImages->get('image_intro');
+
+            $image_intro_alt = $articleImages->get('image_intro_alt', '');
+        }
+
+        // Handle image_fulltext
+        if ($articleImages->get('image_fulltext') > '') {
+            $image_fulltext = strpos($articleImages->get('image_fulltext'), '#') !== false
+                ? substr($articleImages->get('image_fulltext'), 0, strpos($articleImages->get('image_fulltext'), '#'))
+                : $articleImages->get('image_fulltext');
+
+            $image_fulltext_alt = $articleImages->get('image_fulltext_alt', '');
+        }
+
+        return [
+            'image_intro' => $image_intro,
+            'image_intro_alt' => $image_intro_alt,
+            'image_fulltext' => $image_fulltext,
+            'image_fulltext_alt' => $image_fulltext_alt,
+        ];
+    }
+
+
+
+    /**
+     * Extract OG tags from a parameter source (article, menu)
+     *
+     * @param Registry $params The source of OG field mappings (e.g. article attribs, menu params)
+     * @param array &$ogTags Reference to the OG tags array to populate
+     *
+     * @return void
+     */
+    private function getOgTagsFromParams(Registry $params, array &$ogTags): void
     {
 
 
-        $this->setMetaData($document, 'og:title', $params->get('og_title'), 'property');
-        $this->setMetaData($document, 'og:description', $params->get('og_description'), 'property');
-        $this->setMetaData($document, 'og:type', $params->get('og_type'), 'property');
+        foreach (array_keys($ogTags) as $ogTagName) {
+
+            if ($params->exists($ogTagName)) {
+
+                $value = $params->get($ogTagName);
 
 
-        $this->setMetaData($document, 'twitter:card', $params->get('twitter_card'), 'name');
-        $this->setMetaData($document, 'twitter:title', $params->get('twitter_title'), 'name');
-        $this->setMetaData($document, 'twitter:description', $params->get('twitter_description'), 'name');
+                if ($value) {
+                    $ogTags[$ogTagName] = $value;
+                }
+            }
+        }
+    }
 
-        $this->setMetaData($document, 'fb:app_id', $params->get('fb_app_id'), 'property');
+    /**
+     * Get Twitter tags if not set use OG value
+     * @param array &$ogTags
+     *
+     * @return void
+     */
+    private function getTwitterOgTags(array &$ogTags): void
+    {
+        $twitterTags = [
+            'twitter_title' => $ogTags['twitter_title'],
+            'twitter_description' => $ogTags['twitter_description'],
+            'twitter_image' => $ogTags['twitter_image'],
+            'twitter_image_alt' => $ogTags['twitter_image_alt']
+        ];
+        foreach ($twitterTags as $key => $value) {
+            // If the value is not set, use the OG value
+            if (!$value || $value === '') {
+                $ogTags[$key] = $ogTags['og_' . substr($key, 8)];
+            }
+        }
+    }
 
-        $this->setOpenGraphImage(
-            $document,
-            $params->get('og_image'),
-            $params->get('og_image_alt'),
-            Uri::base()
-        );
+    /**
+     * Inject the OpenGraph data into the document.
+     *
+     * @param Document $document
+     * @param array $ogTags
+     *
+     * @return void
+     */
+    private function injectOpenGraphData(Document $document, array $ogTags): void
+    {
+        // OpenGraph tags
+        $this->setMetaData($document, 'og:title', $ogTags['og_title'], 'property');
+        $this->setMetaData($document, 'og:description', $ogTags['og_description'], 'property');
+        $this->setMetaData($document, 'og:type', $ogTags['og_type'], 'property');
+        $this->setMetaData($document, 'og:url', $ogTags['og_url'], 'property');
+
+        // Twitter tags
+        $this->setMetaData($document, 'twitter:card', $ogTags['twitter_card'], 'name');
+        $this->setMetaData($document, 'twitter:title', $ogTags['twitter_title'], 'name');
+        $this->setMetaData($document, 'twitter:description', $ogTags['twitter_description'], 'name');
+
+        // Facebook App ID
+        $this->setMetaData($document, 'fb:app_id', $ogTags['fb_app_id'], 'property');
+
+        $this->setOpenGraphImage($document, $ogTags);
     }
 
     /**
@@ -211,27 +474,53 @@ final class Opengraph extends CMSPlugin implements SubscriberInterface
     }
 
     /**
-     * Set OpenGraph Image tag in document.
+     * Add OpenGraph image to document
      *
-     * @param Document $document
-     * @param string|null $image
-     * @param string|null $alt
-     * @param string|null $baseUrl
+     * @param   Document  $document
+     * @param   array     $ogTags
      *
      * @return void
+     * @since  __DEPLOY_VERSION__
      */
-    private function setOpenGraphImage(Document $document, ?string $image, ?string $alt = '', ?string $baseUrl = ''): void
-    {
-        if (empty($image)) {
+    private function setOpenGraphImage(
+        Document $document,
+        array $ogTags
+    ): void {
+        $image = $ogTags['og_image'];
+        $alt = $ogTags['og_image_alt'];
+        $baseUrl = $ogTags['base_url'];
+        $twitterImage = $ogTags['twitter_image'];
+        $twitterImageAlt = $ogTags['twitter_image_alt'];
+
+        if (empty($image) || !empty($document->getMetaData('og:image'))) {
             return;
         }
 
-        $url = rtrim((string) $baseUrl, '/') . '/' . ltrim($image, '/');
+        $image = preg_replace('~^([\w\-./\\\]+).*$~', '$1', $image);
 
-        $this->setMetaData($document, 'og:image', $url, 'property');
+        if (!file_exists($image)) {
+            return;
+        }
+
+        $ogImageUrl = empty($baseUrl) ? '' : rtrim($baseUrl, '/') . '/';
+        $ogImageUrl .= $image;
+
+        $twitterImageUrl = empty($baseUrl) ? '' : rtrim($baseUrl, '/') . '/';
+        $twitterImageUrl .= $twitterImage;
+
+        $this->setMetaData($document, 'og:image', $ogImageUrl, 'property');
+        $this->setMetaData($document, 'og:image:secure_url', $ogImageUrl, 'property');
         $this->setMetaData($document, 'og:image:alt', $alt, 'property');
-        $this->setMetaData($document, 'twitter:image', $url, 'name');
-        $this->setMetaData($document, 'twitter:image:alt', $alt, 'name');
+        $this->setMetaData($document, 'twitter:image', $twitterImageUrl, 'name');
+        $this->setMetaData($document, 'twitter:image:alt', $twitterImageAlt, 'name');
+
+        $info = getimagesize($image);
+
+        if (\is_array($info)) {
+            $this->setMetaData($document, 'og:image:type', $info['mime'], 'property');
+            $this->setMetaData($document, 'og:image:height', $info[1], 'property');
+            $this->setMetaData($document, 'og:image:width', $info[0], 'property');
+        }
     }
 
 
@@ -273,6 +562,15 @@ final class Opengraph extends CMSPlugin implements SubscriberInterface
     }
 
 
+    /**
+     * Adjust the fields group in the XML file
+     *
+     * @param string $filePath
+     * @param string $newGroup
+     *
+     * @return string
+     * @since  __DEPLOY_VERSION__
+     */
     private function adjustFieldsGroup(string $filePath, string $newGroup): string
     {
         $xmlContent = file_get_contents($filePath);
@@ -288,5 +586,25 @@ final class Opengraph extends CMSPlugin implements SubscriberInterface
         }
 
         return $xml->asXML();
+    }
+
+
+    /**
+     * Get the parameters associated with the active menu item
+     *
+     * @return Registry
+     * @since  __DEPLOY_VERSION__
+     */
+    private function getMenuParams(): Registry
+    {
+        $menu = $this->app->getMenu();
+
+        $active = $menu?->getActive();
+
+        if (!$active instanceof MenuItem) {
+            return new Registry();
+        }
+
+        return $menu->getParams($active->id);
     }
 }
