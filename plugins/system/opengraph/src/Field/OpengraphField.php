a8be0cd5fc00057740df63e7a65bf7e38361bd33
shahzan01 <m.shahzan2003@gmail.com>
2025-08-15 09:35:51 +0530

    fix : changed component name to use dynamic value

--- plugins/system/opengraph/src/Field/OpengraphField.php
+++ plugins/system/opengraph/src/Field/OpengraphField.php
@@ -103,7 +103,7 @@ class OpengraphField extends GroupedlistField
 
 
 
-        if (!$component instanceof FieldsServiceInterface) {
+        if (!$cmp instanceof FieldsServiceInterface) {
             return $groups;
         }
 
