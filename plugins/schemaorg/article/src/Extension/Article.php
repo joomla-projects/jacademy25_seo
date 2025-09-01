0ce3cc89bb5c3ad1550fa394e5ab48681be38b74
shahzan01 <m.shahzan2003@gmail.com>
2025-09-01 19:20:42 +0530

    Work in progress : Added contact selection in Schema.org

--- plugins/schemaorg/article/src/Extension/Article.php
+++ plugins/schemaorg/article/src/Extension/Article.php
@@ -10,11 +10,14 @@
 
 namespace Joomla\Plugin\Schemaorg\Article\Extension;
 
+use Joomla\CMS\Document\Factory;
+use Joomla\CMS\Event\Model\PrepareFormEvent;
 use Joomla\CMS\Event\Plugin\System\Schemaorg\BeforeCompileHeadEvent;
 use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\CMS\Schemaorg\SchemaorgPluginTrait;
 use Joomla\CMS\Schemaorg\SchemaorgPrepareDateTrait;
 use Joomla\CMS\Schemaorg\SchemaorgPrepareImageTrait;
+use Joomla\Database\DatabaseInterface;
 use Joomla\Event\Priority;
 use Joomla\Event\SubscriberInterface;
 
@@ -32,6 +35,14 @@ final class Article extends CMSPlugin implements SubscriberInterface
     use SchemaorgPluginTrait;
     use SchemaorgPrepareDateTrait;
     use SchemaorgPrepareImageTrait;
+
+
+    /**
+     * The application object.
+     *
+     * @var CMSApplication
+     */
+    protected $app;
 
     /**
      * Load the language file on instantiation.
@@ -79,7 +90,41 @@ final class Article extends CMSPlugin implements SubscriberInterface
 
         $graph = $schema->get('@graph');
 
+        $app = $this->app;
+        $contactId = null;
+
+        foreach ($graph as $entry) {
+            // Loop through each property inside the entry
+            foreach ($entry as $key => $value) {
+                // If the property itself is an array (like author, organiser etc.)
+                if (is_array($value)) {
+                    if (isset($value['contact'])) {
+                        $contactId = $value['contact'];
+                        // You can break out here if you only need the first contact
+                        break 2;
+                    }
+                }
+            }
+        }
+
+        $contact = null;
+        if ($contactId) {
+            /** @var MVCComponent $component */
+            $component = $app->bootComponent('com_contact');
+            /** @var MVCFactoryInterface $mvcFactory */
+            $mvcFactory = $component->getMVCFactory();
+            /** @var ContactModel $model */
+            $contactModel = $mvcFactory->createModel('Contact', 'Site', ['ignore_request' => true]);
+
+            $contactModel->setState('params', \Joomla\CMS\Factory::getApplication()->getParams());
+            $contactModel->setState('contact.id', 1);
+
+
+            $contact = $contactModel->getItem();
+        }
+
         foreach ($graph as &$entry) {
+
             if (!isset($entry['@type']) || $entry['@type'] !== 'Article') {
                 continue;
             }
@@ -94,6 +139,30 @@ final class Article extends CMSPlugin implements SubscriberInterface
 
             if (!empty($entry['image'])) {
                 $entry['image'] = $this->prepareImage($entry['image']);
+            }
+            if ($contact) {
+                // Make sure author exists first
+                if (empty($entry['author'])) {
+                    $entry['author'] = [
+                        '@type' => 'Person'
+                    ];
+                }
+
+                if (empty($entry['author']['name']) && !empty($contact->name)) {
+                    $entry['author']['name'] = $contact->name;
+                }
+
+                if (empty($entry['author']['email']) && !empty($contact->email_to)) {
+                    $entry['author']['email'] = $contact->email_to;
+                }
+
+                if (empty($entry['author']['url']) && !empty($contact->webpage)) {
+                    $entry['author']['url'] = $contact->webpage;
+                }
+
+                if (empty($entry['author']['address']) && !empty($contact->address)) {
+                    $entry['author']['address'] = $contact->address;
+                }
             }
         }
 
